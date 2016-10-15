98b394e92c3f67aa8b06aeba85f7ce40bbacb852
Matthew Ramina <mattramina@gmail.com>
2016-10-15 16:21:11 -0400

    use correct method for entity reducer

--- src/ducks/entities.js
+++ src/ducks/entities.js
@@ -1,8 +1,9 @@
 // import assign from "lodash/assign";
+import defaultsDeep from "lodash/defaultsDeep";
 
 const reducer = (state = {user: {}, team: {}, note: {}}, action) => {
     if (action.response && action.response.entities) {
-        return {...state, ...action.response.entities};
+        return defaultsDeep({}, action.response.entities, state);
     }
     return state;
 };
