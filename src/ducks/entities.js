5eaf69d2f2ffa32485c93d3ae388bdb65cd07bda
Matthew Ramina <mattramina@gmail.com>
2016-10-15 21:00:43 -0400

    manual behavior for entity reducer

--- src/ducks/entities.js
+++ src/ducks/entities.js
@@ -1,9 +1,12 @@
-// import assign from "lodash/assign";
-import defaultsDeep from "lodash/defaultsDeep";
-
 const reducer = (state = {user: {}, team: {}, note: {}}, action) => {
     if (action.response && action.response.entities) {
-        return defaultsDeep({}, action.response.entities, state);
+        var newState = {...state};
+        for (const type of Object.keys(action.response.entities)) {
+            for (const ent of Object.keys(action.response.entities[type])){
+                newState[type][ent] = action.response.entities[type][ent];
+            }
+        }
+        return newState;
     }
     return state;
 };
