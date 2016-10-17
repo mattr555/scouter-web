cba2e830a9d1ba446c9c6f03bda3699c11fc411b
Matthew Ramina <mattramina@gmail.com>
2016-10-17 15:30:54 -0400

    add ability to move schema fields up and down
    fix handling of option fields to make them use arrays

--- src/components/SettingsPage/SchemaBuilder.js
+++ src/components/SettingsPage/SchemaBuilder.js
@@ -2,23 +2,29 @@ import React from "react";
 import {Table, Button} from "react-bootstrap";
 import {connect} from "react-redux";
 import SchemaBuilderRow from "./SchemaBuilderRow";
-import {changeField, deleteField, addField} from "ducks/schemaBuilder";
+import {changeField, deleteField, addField, moveFieldUp, moveFieldDown} from "ducks/schemaBuilder";
 
 class SchemaBuilder extends React.Component {
     static propTypes = {
         schema: React.PropTypes.array,
         onFieldChange: React.PropTypes.func,
         onFieldDelete: React.PropTypes.func,
-        onFieldAdd: React.PropTypes.func
+        onFieldAdd: React.PropTypes.func,
+        onFieldUp: React.PropTypes.func,
+        onFieldDown: React.PropTypes.func
     }
 
     render() {
-        var rows = this.props.schema.map((r) => (
+        var rows = this.props.schema.map((r, i) => (
             <SchemaBuilderRow
                 key={r.id}
                 data={r}
                 onChange={this.props.onFieldChange}
-                onDelete={this.props.onFieldDelete} />
+                onDelete={this.props.onFieldDelete}
+                onUp={this.props.onFieldUp}
+                onDown={this.props.onFieldDown}
+                top={i === 0}
+                bottom={i === this.props.schema.length - 1} />
         ));
 
         return <div>
@@ -46,7 +52,9 @@ const mapDispatchToProps = (dispatch) => {
     return {
         onFieldChange: (field) => dispatch(changeField(field)),
         onFieldDelete: (id) => dispatch(deleteField(id)),
-        onFieldAdd: () => dispatch(addField())
+        onFieldAdd: () => dispatch(addField()),
+        onFieldUp: (id) => dispatch(moveFieldUp(id)),
+        onFieldDown: (id) => dispatch(moveFieldDown(id))
     };
 };
 
--- src/components/SettingsPage/SchemaBuilderRow.js
+++ src/components/SettingsPage/SchemaBuilderRow.js
@@ -1,11 +1,19 @@
 import React from "react";
 import {FormControl, Button, Glyphicon} from "react-bootstrap";
 
+const disabledStyle = {
+    opacity: 0
+};
+
 class SchemaBuilderRow extends React.Component {
     static propTypes = {
         data: React.PropTypes.object,
         onChange: React.PropTypes.func,
-        onDelete: React.PropTypes.func
+        onDelete: React.PropTypes.func,
+        onUp: React.PropTypes.func,
+        onDown: React.PropTypes.func,
+        top: React.PropTypes.bool,
+        bottom: React.PropTypes.bool
     }
 
     // state = {form: {...this.props.data, options: this.props.data.options && this.props.data.options.join(",") || ""}}
@@ -14,22 +22,36 @@ class SchemaBuilderRow extends React.Component {
         // this.setState({
         //     form: {...this.state.form, [event.target.name]: event.target.value}
         // });
-        this.props.onChange({...this.props.data, [event.target.name]: event.target.value});
+        const newForm = {...this.props.data, [event.target.name]: event.target.value};
+        let options = newForm.options;
+        if (newForm.type !== "option") {
+            delete newForm.options;
+            this.props.onChange(newForm);
+            return;
+        } else if (typeof options === "string"){
+            options = options.split(",");
+        } else if (typeof options === "undefined"){
+            options = [];
+        }
+        this.props.onChange({...newForm, options});
     }
 
     onDelete = () => this.props.onDelete(this.props.data.id)
+    onUp = () => this.props.onUp(this.props.data.id)
+    onDown = () => this.props.onDown(this.props.data.id)
 
     render() {
-        const {data} = this.props;
+        const {data, top, bottom} = this.props;
         var optionSelect = "n/a";
 
         if (data.type === "option") {
+            const options = data.options && data.options.join(",") || "";
             optionSelect = (
                 <FormControl
                     type="text"
                     name="options"
                     placeholder="comma separated"
-                    value={data.options}
+                    value={options}
                     onChange={this.onChange} />
             );
         }
@@ -58,6 +80,8 @@ class SchemaBuilderRow extends React.Component {
             </td>
             <td>
                 <Button bsSize="xsmall" bsStyle="danger" onClick={this.onDelete}><Glyphicon glyph="trash"/></Button>
+                <Button bsSize="xsmall" bsStyle="link" onClick={this.onUp} disabled={top} style={top && disabledStyle || {}}><Glyphicon glyph="chevron-up"/></Button>
+                <Button bsSize="xsmall" bsStyle="link" onClick={this.onDown} disabled={bottom} style={bottom && disabledStyle || {}}><Glyphicon glyph="chevron-down"/></Button>
             </td>
         </tr>;
     }
